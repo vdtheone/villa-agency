d60ba99f3b3a9746d7fb2f096e6065691c924504
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-18 17:41:34 +0530

    create for url

--- villa_agency/accounts/urls.py
+++ villa_agency/accounts/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.first_view, name="first_view")
+    path("home", views.home, name="home"),
+    path("properties", views.properties, name="properties"),
+    path("property_details", views.property_details, name="property_details"),
+    path("contact", views.contact, name="contact")
 ]   
