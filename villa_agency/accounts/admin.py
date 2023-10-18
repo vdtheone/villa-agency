d50d7cde6b2d335d692502a02366cb9f2bf249b0
vdtheone <vishalprajapati22052001@gmail.com>
2023-10-18 17:40:50 +0530

    register models

--- villa_agency/accounts/admin.py
+++ villa_agency/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Property
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['type']
+
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ['category','price','image']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Property, PropertyAdmin)
