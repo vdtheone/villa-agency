e0b102de4c34f9ff3f1723297d293ee375dee5f0
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-04 22:53:49 +0530

    edit home url

--- villa_agency/accounts/urls.py
+++ villa_agency/accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("home", views.home, name="home"),
+    path("", views.home, name="home"),
     path("properties", views.properties, name="properties"),
     path("property_details", views.property_details, name="property_details"),
     path("contact", views.contact, name="contact")
