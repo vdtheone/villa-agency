cff15eb348474a41a526a3233950bcb6d15869e7
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-04 22:17:51 +0530

    add views

--- villa_agency/accounts/views.py
+++ villa_agency/accounts/views.py
@@ -1,18 +1,35 @@
-from django.shortcuts import render
-from .models import Property
+from django.shortcuts import redirect, render
+from .models import ContactUs, Property
 
 # Create your views here.
 def home(request):
-    properties = Property.objects.all()
+    properties = Property.objects.all()[0:6]
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        detail = ContactUs(name=name, email=email, subject=subject, message=message)
+        detail.save()
+        redirect('/home')
     return render(request, 'home.html', {'properties':properties})
 
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        detail = ContactUs(name=name, email=email, subject=subject, message=message)
+        detail.save()
+        redirect('/home')
     return render(request, 'contact.html')
 
 
 def properties(request):
-    return render(request, 'properties.html')
+    properties = Property.objects.all()
+    return render(request, 'properties.html', {'properties':properties})
 
 
 def property_details(request):
