4738e6a9182b050defa408bc21379e2a90ab30b8
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-04 23:50:08 +0530

    change in static/ to /static/

--- villa_agency/villa_agency/settings.py
+++ villa_agency/villa_agency/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATICFILES_DIRS = [
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
 ]
 
 
-# MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/' 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
