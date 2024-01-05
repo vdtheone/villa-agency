8817851147cab9c544730003dc7ecfe78f1ac49e
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-05 09:39:44 +0530

    all "*" in allowed host

--- villa_agency/villa_agency/settings.py
+++ villa_agency/villa_agency/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-4hvwu&4_$=2s^+ie3-1#vkgv(b(4jr4v7tww5ub!e%d)^*cwuh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,12 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 
 # MEDIA_URL = '/media/' 
